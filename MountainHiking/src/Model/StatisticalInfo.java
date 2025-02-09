a40088892144bd2366492578b92ff8ab25c8d526
LeHuyTuong <60842410+LeHuyTuong@users.noreply.github.com>
2025-02-09 22:27:10 +0700

    Update StatisticalInfo.java

--- MountainHiking/src/Model/StatisticalInfo.java
+++ MountainHiking/src/Model/StatisticalInfo.java
@@ -5,6 +5,8 @@
  */
 package Model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.List;
 
 /**
@@ -12,38 +14,60 @@ import java.util.List;
  * @author USER
  */
 public class StatisticalInfo {
-    private String mountainCode;
+    private String mountainName;
     private int numOfStudent;
     private double totalCost;
     
     public StatisticalInfo(){
-        
     }
 
 
-    public StatisticalInfo(String mountainCode, List<Student> l, double tutionFee) {
-        this.mountainCode = mountainCode;
+    public StatisticalInfo(String mountainName, List<Student> l, double tutionFee) {
+        this.mountainName = mountainName;
         this.numOfStudent = 1;
         this.totalCost =tutionFee;    }
 
+    @Override
     public String toString() {
-        return String.format("%-14s   | %-14d                  | %,.0f",
+        return String.format("%-20s   | %-14s                  | %,.0f",
             this.mountainName,
             this.numOfStudent,
             this.totalCost
     );
-    public StatisticalInfo(String mountainCoe, int numOfStudent, double totalCost) {
-        this.mountainCode = mountainCoe;
+    }
+    public StatisticalInfo(String mountainName, int numOfStudent, double totalCost) {
+        this.mountainName = mountainName;
         this.numOfStudent = numOfStudent;
         this.totalCost = totalCost;
     }
 
-    public String getMountainCoe() {
-        return mountainCode;
+   
+    public static String getMountainName(String id) {
+        String Name = null;
+        try{
+            FileReader f = new FileReader("MountainList.csv");
+            BufferedReader r = new BufferedReader(f);
+            while(r.ready()){
+                String s = r.readLine();
+                String[] str = s.split(",");
+                if(str.length > 0){
+                    String name = str[1];
+                    if(str[0].trim().equalsIgnoreCase(id.trim())){
+                        Name = str[1].trim();
+                        break;
+                    }
+                }
+            }
+            r.close();
+            f.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return Name;
     }
 
-    public void setMountainCoe(String mountainCoe) {
-        this.mountainCode = mountainCoe;
+    public void setMountainName(String mountainName) {
+        this.mountainName = mountainName;
     }
 
     public int getNumOfStudent() {
