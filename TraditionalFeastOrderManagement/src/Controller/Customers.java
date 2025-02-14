81016317cde23ddf4db8eb32013d7fd7d6ad2b42
LeHuyTuong <60842410+LeHuyTuong@users.noreply.github.com>
2025-02-14 16:25:10 +0700

    Update Customers.java

--- TraditionalFeastOrderManagement/src/Controller/Customers.java
+++ TraditionalFeastOrderManagement/src/Controller/Customers.java
@@ -18,13 +18,15 @@ import java.util.ArrayList;
  * @author USER
  */
 public class Customers {
-    private static ArrayList<Customer> customers;
-
-    public Customers(){
-        customers = new ArrayList<>();
-    }
     
-    public static void saveToFile(String fileSaveName){
+    public static String HEADER_CUSTOMER_INFO = 
+        "--------------------------------------------------------------------------\n"
+       +"Code   |      Customer Name        | Phone     |Email\n"
+       +"--------------------------------------------------------------------------";
+     public static String FOOTER_INFO =
+        "---------------------------------------------------------------------------";
+
+    public static void saveToFile(String fileSaveName, ArrayList<Customer> customers){
         FileOutputStream fos = null;
         try{
             File f = new File(fileSaveName);
@@ -43,6 +45,26 @@ public class Customers {
             }catch(Exception e){
                 System.out.println("Error closing file");
             }
+        }
+    }
+    
+    public static void showData(ArrayList<Customer> customers){
+        boolean check = false;
+        for(Customer c : customers){
+            if(!check){
+                System.out.println(HEADER_CUSTOMER_INFO);
+                check = true;
+            }
+            System.out.printf("%-6s | %-25s |%-10s | %-20s%n",
+                    c.getCustomerCode(),
+                    c.getName(),
+                    c.getPhoneNumber(),
+                    c.getEmail());
+        }
+        if(check){
+            System.out.println(FOOTER_INFO);
+        }else{
+            System.out.println("Does not have any customer information");
         }
     }
 }
